c7066f9f08157fb1bd8779f4899bbc283d6da7f7
Louie Kolozinksi <louisk02@icloud.com>
2024-05-23 17:44:17 +1200

    Initial Commit

---
+++ Idle Clicker/Idle_ClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Idle_ClickerApp.swift
+//  Idle Clicker
+//
+//  Created by Louis Kolodzinski on 23/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Idle_ClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
