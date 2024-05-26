8a21a48c844b8ed94642be1b1e9dc794674ce99a
Louie Kolozinksi <louisk02@icloud.com>
2024-05-26 21:04:47 +1200

    -Fixed userPoints duplication bug -Reworked userPoints to be able to handle large numbers over 10 to the power of 16 "10 Quintillion" (note: the ability to purchase a pointsGenerator upgrade breaks when genPrice < 10 Quintillion) -Added pointsGeneratorView to handle pointsGenerator to have more control over UI -Added a 'prestige' system to pointsGenerator

---
+++ Idle Clicker/CustomUI/PointGeneratorView.swift
@@ -0,0 +1,8 @@
+//
+//  PointGeneratorView.swift
+//  Idle Clicker
+//
+//  Created by Louis Kolodzinski on 26/05/2024.
+//
+
+import Foundation
---
+++ Idle Clicker/Models/DataModels/LargeNumber.swift
@@ -0,0 +1,8 @@
+//
+//  LargeNumber.swift
+//  Idle Clicker
+//
+//  Created by Louis Kolodzinski on 26/05/2024.
+//
+
+import Foundation
